0f15a3be764cce9af15edb61bc598016795faa75
Mike Porter <michael.porter@vivintsolar.com>
2020-02-16 17:11:36 -0700

    add readme, change ring_cw and ring_ccw to use PWM

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-# A blank Mongoose OS app
+# TARDIS - the app!
 
 ## Overview
 
-This is an empty app, serves as a skeleton for building Mongoose OS
-apps from scratch.
+This project began life several years ago when my sone gave me a 3D-printed TARDIS and some LEDs.
 
+Not content with just lighting up a bunch of LEDs "it works fine if you just hook them up in series dad, the voltage drop across the LEDs is enough to power them all..."
+
+Instead, I wanted them to be able to be individually powered up.  I've a had a couple of microcontrollers for years (Basic Stamp and a Teensy USB), but I quickly realized that just being able to light the LEDs up in sequence is no longer enough.  I want them to REACT to things.
+
+I also have a couple of OrangePI PCs, so I thought about running something from those.  The attractivness of the OrangePI approach is the fact that these Rasberry PI clones have a significant amount of computation power.  However, the space available was not particularly conducive to such a large board.  In addition, the power requirements and heat generation are less than desirable.  Putting the computer outside the Tardis enclosure (sorry Dr. Who fans, my Tardis is not actually bigger on the inside...) meant I would have to run at least 6 wires to have individual control of the LEDs; this would lead to un unsightly mess of wires.
+
+I decided that I wanted something that could be very portable and possibly run on batteries.
+
+Enter the ESP32.  At around that time, I discovered the ESP series from Espressif, but I had a really hard time justifying spending much on something that is objectively little more than a toy.
+
+However, the NodeMCU dev boards based on either the ESP8266 or ESP32 are pretty powerful.  Then I stumbled across MongooseOS, and I realized that I had the complete package I needed.  Mongoose provides a powerful library of IOT features including shadow support, OTA updates, and for a hobbyist, the price is fantastic.  Free for the OS (with the simple restriction that the "Free" OS does not allow OTA updates except via their dashboard), and free for the dashboard, up to 10 devices.  mDash fully supports OTA configuration edits, OTA updates, shadow, and even provide database accessibility.  Mongoose itself provides support for AWS, Google, Azure, Samsung, their own mDash, basically, a complete package, and so, the Tardis project was born.
---
+++ fs/init.js
@@ -0,0 +1,242 @@
+load('api_aws.js');
+load('api_config.js');
+load('api_dash.js');
+load('api_events.js');
+load('api_gpio.js');
+load('api_mqtt.js');
+load('api_shadow.js');
+load('api_timer.js');
+load('api_sys.js');
+load('api_pwm.js');
+
+let btn = Cfg.get('board.btn1.pin');              // Built-in button GPIO
+let ledT = Cfg.get('board.ledT.pin');               // LED for TOP
+let ledA = Cfg.get('board.ledA.pin');
+let ledB = Cfg.get('board.ledB.pin');
+let ledC = Cfg.get('board.ledC.pin');
+let ledD = Cfg.get('board.ledD.pin');
+
+let onhiT = Cfg.get('board.ledT.active_high');     // LED on when high?
+let onhiA = Cfg.get('board.ledA.active_high');     // LED on when high?
+let onhiB = Cfg.get('board.ledB.active_high');     // LED on when high?
+let onhiC = Cfg.get('board.ledC.active_high');     // LED on when high?
+let onhiD = Cfg.get('board.ledD.active_high');     // LED on when high?
+
+let AlertStack = [];
+let state = {alert: "none", btnCount: 0, uptime: 0, "stack": AlertStack};  // Device state
+let online = false;                               // Connected to the cloud?
+
+print('********** reached end of config load **********');
+
+let setLED = function(led, state, onhi) {
+  let level = onhi ? state : !state;
+  GPIO.set_mode(led, GPIO.MODE_OUTPUT);
+  GPIO.write(led, level);
+};
+
+let br_step = 1;
+let br_dir = 1;
+
+let breathe = function() {
+  // print('breath: ', br_step, 'dir: ', br_dir)
+  PWM.set(ledT, 60, br_step * 0.03 );
+  if (br_step === 28) { br_dir = -1 };
+  if (br_step === 1) { br_dir = 1};
+  br_step = br_step + br_dir;
+};
+
+
+let ring_step = 0;
+
+let clockwise = function() {
+  if (ring_step === 0) {
+    PWM.set(ledA, 60, 1);
+    PWM.set(ledB, 0, 0);
+    PWM.set(ledC, 0, 0);
+    PWM.set(ledD, 60, .5);
+
+    ring_step = 1;
+  } else if (ring_step === 1) {
+    PWM.set(ledA, 60, .5);
+    PWM.set(ledB, 60, 1);
+    PWM.set(ledC, 0, 0);
+    PWM.set(ledD, 0, 0);
+    ring_step = 2;
+  } else if (ring_step === 2) {
+    PWM.set(ledA, 0, 0);
+    PWM.set(ledB, 60, .5);
+    PWM.set(ledC, 60, 1);
+    PWM.set(ledD, 0, 0);
+    ring_step = 3;
+  } else if (ring_step === 3) {
+    PWM.set(ledA, 0, 0);
+    PWM.set(ledB, 0, 0);
+    PWM.set(ledC, 60, .5);
+    PWM.set(ledD, 60, 1);
+    ring_step = 0;
+  }
+
+};
+
+let counterclockwise = function() {
+  if (ring_step === 0) {
+    PWM.set(ledA, 60, 1);
+    PWM.set(ledB, 60, .5);
+    PWM.set(ledC, 0, 0);
+    PWM.set(ledD, 0, 0);
+    ring_step = 1;
+  } else if (ring_step === 1) {
+    PWM.set(ledA, 60, .5);
+    PWM.set(ledB, 60, 0);
+    PWM.set(ledC, 0, 0);
+    PWM.set(ledD, 60, 1);
+    ring_step = 2;
+  } else if (ring_step === 2) {
+    PWM.set(ledA, 0, 0);
+    PWM.set(ledB, 0, 0);
+    PWM.set(ledC, 60, 1);
+    PWM.set(ledD, 60, .5);
+    ring_step = 3;
+  } else if (ring_step === 3) {
+    PWM.set(ledA, 0, 0);
+    PWM.set(ledB, 60, 1);
+    PWM.set(ledC, 60, .5);
+    PWM.set(ledD, 0, 0);
+    ring_step = 0;
+  }
+
+};
+
+let ring = Timer.set(1, 0, function() {print('ring init');}, null);
+
+let breath = Timer.set(200, Timer.REPEAT, breathe, null);
+
+
+
+let setAlert = function(alert) {
+  print('alert state is now: ', state.alert);
+  // clear all alert statuses when setting a new alert state
+  Timer.del(breath);
+  Timer.del(ring);
+  PWM.set(ledT, 0, br_step)
+  GPIO.blink(ledT, 0, 0);
+  GPIO.blink(ledA, 0, 0);
+  GPIO.blink(ledB, 0, 0);
+  GPIO.blink(ledC, 0, 0);
+  GPIO.blink(ledD, 0, 0);
+  setLED(ledT, false, onhiT);
+  setLED(ledA, false, onhiA);
+  setLED(ledB, false, onhiB);
+  setLED(ledC, false, onhiC);
+  setLED(ledD, false, onhiD);
+  if (state.alert === "test") {
+    print("Caugh test state!");
+  } else if (state.alert === "blink_top") {
+    GPIO.blink(ledT, 500, 500);
+  } else if (state.alert === "blink_ring") {
+    GPIO.blink(ledA, 500, 500);
+    GPIO.blink(ledB, 500, 500);
+    GPIO.blink(ledC, 500, 500);
+    GPIO.blink(ledD, 500, 500);
+  } else if (state.alert === "ring_cw") {
+    ring = Timer.set(1000, Timer.REPEAT, clockwise, null);
+  } else if (state.alert === "ring_ccw") {
+    ring = Timer.set(1000, Timer.REPEAT, counterclockwise, null);
+  } else {
+    breath = Timer.set(200, Timer.REPEAT, breathe, null)  ;
+  }
+};
+
+setLED(ledT, state.top, onhiT);
+setLED(ledA, state.ledA, onhiA);
+setLED(ledB, state.ledB, onhiB);
+setLED(ledC, state.ledC, onhiC);
+setLED(ledD, state.ledD, onhiD);
+
+let reportState = function() {
+  Shadow.update(0, state);
+};
+
+// Update state every minute, and report to cloud if online
+Timer.set(60000, Timer.REPEAT, function() {
+  state.uptime = Sys.uptime();
+  state.ram_free = Sys.free_ram();
+  print('online:', online, JSON.stringify(state));
+  if (online) reportState();
+}, null);
+
+// Set up Shadow handler to synchronise device state with the shadow state
+Shadow.addHandler(function(event, obj) {
+  if (event === 'UPDATE_DELTA') {
+    print('GOT DELTA:', JSON.stringify(obj));
+    for (let key in obj) {  // Iterate over all keys in delta
+      if (key === 'on') {   // We know about the 'on' key. Handle it!
+        state.on = obj.on;  // Synchronise the state
+        setLED(ledT, state.on, onhiT);   // according to the delta
+      } else if (key === 'top') {   // We know about the 'on' key. Handle it!
+        state.top = obj.top;  // Synchronise the state
+        setLED(ledT, state.top, onhiT);   // according to the delta
+      } else if (key === 'ledA') {   // We know about the 'on' key. Handle it!
+        state.ledA = obj.ledA;  // Synchronise the state
+        setLED(ledA, state.ledA, onhiA);   // according to the delta
+      } else if (key === 'ledB') {   // We know about the 'on' key. Handle it!
+        state.ledB = obj.ledB;  // Synchronise the state
+        setLED(ledB, state.ledB, onhiB);   // according to the delta
+      } else if (key === 'ledC') {   // We know about the 'on' key. Handle it!
+        state.ledC = obj.ledC;  // Synchronise the state
+        setLED(ledC, state.ledC, onhiC);   // according to the delta
+      } else if (key === 'ledD') {   // We know about the 'on' key. Handle it!
+        state.ledD = obj.ledD;  // Synchronise the state
+        setLED(ledD, state.ledD, onhiD);   // according to the delta
+      } else if (key === 'alert') {
+        state.alert = obj.alert;
+        setAlert(obj.alert);
+      } else if (key === 'reboot') {
+        state.reboot = obj.reboot;      // Reboot button clicked: that
+        Timer.set(750, 0, function() {  // incremented 'reboot' counter
+          Sys.reboot(500);                 // Sync and schedule a reboot
+        }, null);
+      }
+    }
+    reportState();  // Report our new state, hopefully clearing delta
+  }
+});
+
+if (btn >= 0) {
+  let btnCount = 0;
+  let btnPull, btnEdge;
+  if (Cfg.get('board.btn1.pull_up') ? GPIO.PULL_UP : GPIO.PULL_DOWN) {
+    btnPull = GPIO.PULL_UP;
+    btnEdge = GPIO.INT_EDGE_NEG;
+  } else {
+    btnPull = GPIO.PULL_DOWN;
+    btnEdge = GPIO.INT_EDGE_POS;
+  }
+  GPIO.set_button_handler(btn, btnPull, btnEdge, 20, function() {
+    state.btnCount++;
+    let message = JSON.stringify(state);
+    let sendMQTT = true;
+    if (Dash.isConnected()) {
+      print('== Click!');
+      // TODO: Maybe do something else?
+      sendMQTT = false;
+    }
+    // AWS is handled as plain MQTT since it allows arbitrary topics.
+    if (AWS.isConnected() || (MQTT.isConnected() && sendMQTT)) {
+      let topic = 'devices/' + Cfg.get('device.id') + '/events';
+      print('== Publishing to ' + topic + ':', message);
+      MQTT.pub(topic, message, 0 /* QoS */);
+    } else if (sendMQTT) {
+      print('== Not connected!');
+    }
+  }, null);
+}
+
+Event.on(Event.CLOUD_CONNECTED, function() {
+  online = true;
+  Shadow.update(0, {ram_total: Sys.total_ram()});
+}, null);
+
+Event.on(Event.CLOUD_DISCONNECTED, function() {
+  online = false;
+}, null);
