ca37e2eb4923c24bb9139ae8c54fc5b912fc4072
Mike Porter <michael.porter@vivintsolar.com>
2020-02-20 15:32:03 -0700

    pwm-ify ring_cw and ring_ccw added fast mode added "spiral" modes added provisioning monitor on pin 5/ledT

--- fs/init.js
+++ fs/init.js
@@ -47,71 +47,104 @@ let breathe = function() {
 
 
 let ring_step = 0;
+let ring_sub1 = 5;
+let ring_sub2 = 0;
+let spiral_step = 0;
+let spiral_sub = 1;
+let spiral_dir = 1;
 
 let clockwise = function() {
-  if (ring_step === 0) {
-    PWM.set(ledA, 60, 1);
-    PWM.set(ledB, 0, 0);
-    PWM.set(ledC, 0, 0);
-    PWM.set(ledD, 60, .5);
 
-    ring_step = 1;
+  if (ring_step === 0) {
+    // ledA is increasing, ledD is decreasing
+    PWM.set(ledC, 50, 1);
+    PWM.set(ledB, 50, 1);
+    PWM.set(ledA, 50, --ring_sub1 * 0.20);
+    PWM.set(ledD, 50, ring_sub2++ * 0.15);
   } else if (ring_step === 1) {
-    PWM.set(ledA, 60, .5);
-    PWM.set(ledB, 60, 1);
-    PWM.set(ledC, 0, 0);
-    PWM.set(ledD, 0, 0);
-    ring_step = 2;
+    // ledB is increasing, ledA is decreasing
+    PWM.set(ledC, 50, 1);
+    PWM.set(ledD, 50, 1);
+    PWM.set(ledB, 50, --ring_sub2 * 0.20);
+    PWM.set(ledA, 50, ring_sub1++ * 0.15);
   } else if (ring_step === 2) {
-    PWM.set(ledA, 0, 0);
-    PWM.set(ledB, 60, .5);
-    PWM.set(ledC, 60, 1);
-    PWM.set(ledD, 0, 0);
-    ring_step = 3;
+    // ledC is increasing, ledB is decreasing
+    PWM.set(ledA, 50, 1);
+    PWM.set(ledD, 50, 1);
+    PWM.set(ledC, 50, --ring_sub1 * 0.20);
+    PWM.set(ledB, 50, ring_sub2++ * 0.15);
   } else if (ring_step === 3) {
-    PWM.set(ledA, 0, 0);
-    PWM.set(ledB, 0, 0);
-    PWM.set(ledC, 60, .5);
-    PWM.set(ledD, 60, 1);
-    ring_step = 0;
+    // ledD is increasing, ledC is decreasing
+    PWM.set(ledA, 50, 1);
+    PWM.set(ledB, 50, 1);
+    PWM.set(ledD, 50, --ring_sub2 * 0.20);
+    PWM.set(ledC, 50, ring_sub1++ * 0.15);
   }
+  if ( ( ring_sub1 <= 0 ) || (ring_sub1 >= 5) ) { print('step: ', ring_step++);}
+  if ( ring_step >= 4 ) { ring_step = 0 };
 
 };
 
 let counterclockwise = function() {
   if (ring_step === 0) {
-    PWM.set(ledA, 60, 1);
-    PWM.set(ledB, 60, .5);
-    PWM.set(ledC, 0, 0);
-    PWM.set(ledD, 0, 0);
-    ring_step = 1;
+    // ledD is increasing, ledA is decreasing
+    PWM.set(ledC, 50, 1);
+    PWM.set(ledB, 50, 1);
+    PWM.set(ledD, 50, --ring_sub1 * 0.20);
+    PWM.set(ledA, 50, ring_sub2++ * 0.15);
   } else if (ring_step === 1) {
-    PWM.set(ledA, 60, .5);
-    PWM.set(ledB, 60, 0);
-    PWM.set(ledC, 0, 0);
-    PWM.set(ledD, 60, 1);
-    ring_step = 2;
+    // ledC is increasing, ledD is decreasing
+    PWM.set(ledA, 50, 1);
+    PWM.set(ledB, 50, 1);
+    PWM.set(ledC, 50, --ring_sub2 * 0.20);
+    PWM.set(ledD, 50, ring_sub1++ * 0.15);
   } else if (ring_step === 2) {
-    PWM.set(ledA, 0, 0);
-    PWM.set(ledB, 0, 0);
-    PWM.set(ledC, 60, 1);
-    PWM.set(ledD, 60, .5);
-    ring_step = 3;
+    // ledB is increasing, ledC is decreasing
+    PWM.set(ledA, 50, 1);
+    PWM.set(ledD, 50, 1);
+    PWM.set(ledB, 50, --ring_sub1 * 0.20);
+    PWM.set(ledC, 50, ring_sub2++ * 0.15);
   } else if (ring_step === 3) {
-    PWM.set(ledA, 0, 0);
-    PWM.set(ledB, 60, 1);
-    PWM.set(ledC, 60, .5);
-    PWM.set(ledD, 0, 0);
-    ring_step = 0;
+    // ledA is increasing, ledB is decreasing
+    PWM.set(ledC, 50, 1);
+    PWM.set(ledD, 50, 1);
+    PWM.set(ledA, 50, --ring_sub2 * 0.20);
+    PWM.set(ledB, 50, ring_sub1++ * 0.15);
   }
+  if ( ( ring_sub1 <= 0 ) || (ring_sub1 >= 5) ) { print('step: ', ring_step++);}
+  if ( ring_step >= 4 ) { ring_step = 0 };
 
 };
 
 let ring = Timer.set(1, 0, function() {print('ring init');}, null);
 
-let breath = Timer.set(200, Timer.REPEAT, breathe, null);
+let breath = Timer.set(1, 0, function() {print('breath init');}, null);
 
+let spiral_cw = function() {
+  if (spiral_step <= 0 ) {PWM.set(ledA, 50, spiral_sub * 0.25); spiral_step++; }
+  else if (spiral_step === 1 ) {PWM.set(ledB, 50, spiral_sub * 0.25); spiral_step++; }
+  else if (spiral_step === 2 ) {PWM.set(ledC, 50, spiral_sub * 0.25); spiral_step++; }
+  else if (spiral_step >= 3 ){
+    PWM.set(ledD, 50, spiral_sub * 0.25);
+    spiral_step = 0;
+    spiral_sub = spiral_sub + spiral_dir;
+    if (spiral_sub >= 4) { spiral_dir = -1 }
+    else if (spiral_sub <= 0) { spiral_dir = 1 }
+  }
+};
 
+let spiral_ccw = function() {
+  if (spiral_step === 0) {PWM.set(ledD, 50, spiral_sub * 0.25); spiral_step++;}
+  else if (spiral_step === 1) {PWM.set(ledC, 50, spiral_sub * 0.25); spiral_step++;}
+  else if (spiral_step === 2) {PWM.set(ledB, 50, spiral_sub * 0.25); spiral_step++;}
+  else {
+    PWM.set(ledA, 50, spiral_sub * 0.25);
+    spiral_step = 0;
+    spiral_sub = spiral_sub + spiral_dir;
+    if (spiral_sub >= 4) { spiral_dir = -1 }
+    else if (spiral_sub <= 0) { spiral_dir = 1 }
+  }
+};
 
 let setAlert = function(alert) {
   print('alert state is now: ', state.alert);
@@ -129,6 +162,11 @@ let setAlert = function(alert) {
   setLED(ledB, false, onhiB);
   setLED(ledC, false, onhiC);
   setLED(ledD, false, onhiD);
+  PWM.set(ledT, 0, 0);
+  PWM.set(ledA, 0, 0);
+  PWM.set(ledB, 0, 0);
+  PWM.set(ledC, 0, 0);
+  PWM.set(ledD, 0, 0);
   if (state.alert === "test") {
     print("Caugh test state!");
   } else if (state.alert === "blink_top") {
@@ -139,15 +177,26 @@ let setAlert = function(alert) {
     GPIO.blink(ledC, 500, 500);
     GPIO.blink(ledD, 500, 500);
   } else if (state.alert === "ring_cw") {
-    ring = Timer.set(1000, Timer.REPEAT, clockwise, null);
+    ring = Timer.set(100, Timer.REPEAT, clockwise, null);
   } else if (state.alert === "ring_ccw") {
-    ring = Timer.set(1000, Timer.REPEAT, counterclockwise, null);
+    ring = Timer.set(100, Timer.REPEAT, counterclockwise, null);
+  } else if (state.alert === "ring_cw_fast") {
+    ring = Timer.set(50, Timer.REPEAT, clockwise, null);
+  } else if (state.alert === "ring_ccw_fast") {
+    ring = Timer.set(50, Timer.REPEAT, counterclockwise, null);
+  } else if (state.alert === "spiral_cw") {
+    ring = Timer.set(200, Timer.REPEAT, spiral_cw, null);
+  } else if (state.alert === "spiral_ccw") {
+    ring = Timer.set(200, Timer.REPEAT, spiral_ccw, null);
+  } else if (state.alert === "spiral_cw_fast") {
+    ring = Timer.set(50, Timer.REPEAT, spiral_cw, null);
+  } else if (state.alert === "spiral_ccw_fast") {
+    ring = Timer.set(50, Timer.REPEAT, spiral_ccw, null);
   } else {
     breath = Timer.set(200, Timer.REPEAT, breathe, null)  ;
   }
 };
 
-setLED(ledT, state.top, onhiT);
 setLED(ledA, state.ledA, onhiA);
 setLED(ledB, state.ledB, onhiB);
 setLED(ledC, state.ledC, onhiC);
@@ -235,6 +284,10 @@ if (btn >= 0) {
 Event.on(Event.CLOUD_CONNECTED, function() {
   online = true;
   Shadow.update(0, {ram_total: Sys.total_ram()});
+  // I use ledT to show provisioning state, once I'm CLOUD_CONNECTED,
+  //    I don't really need it for that anymore...
+  setLED(ledT, state.top, onhiT);
+
 }, null);
 
 Event.on(Event.CLOUD_DISCONNECTED, function() {
